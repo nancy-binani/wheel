e13828c573b6d32f208368e38a0d4fa7b5946f51
Nancy Binani <nancybinani1207@gmail.com>
2022-09-21 18:58:56 +0530

    Added delete note ui

--- app/javascript/src/components/Dashboard/Notes/Note/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/Note/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { MenuVertical, Clock } from "neetoicons";
 import { Typography, Dropdown, Button, Avatar } from "neetoui";
 
-const Note = ({ notes }) =>
+const Note = ({ notes, handleDelete }) =>
   notes.map(note => (
     <>
       <div className="border-slate-300  shadow-slate-300 box-border h-40 w-full items-start rounded-sm border-2 bg-white p-4 shadow-md">
@@ -11,7 +11,7 @@ const Note = ({ notes }) =>
           <Typography style="h5">{note.title}</Typography>
           <Dropdown buttonStyle="text" icon={MenuVertical}>
             <li>Edit</li>
-            <li>Delete</li>
+            <li onClick={handleDelete}>Delete</li>
           </Dropdown>
         </div>
         <Typography className="mb-3 text-gray-500" style="body2">
