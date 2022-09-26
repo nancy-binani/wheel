ecd2f525a239d6e994abfeded4cc913a2e4c29b5
Nancy Binani <nancybinani1207@gmail.com>
2022-09-26 17:24:40 +0530

    Renamed components

--- app/javascript/src/components/Dashboard/Notes/Pane/Create.jsx
+++ app/javascript/src/components/Dashboard/Notes/Pane/Create.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 
-import { Pane as NeetoUIPane, Typography } from "neetoui";
+import { Pane, Typography } from "neetoui";
 
 import Form from "./Form";
 
 import { NOTES_FORM_INITIAL_FORM_VALUES } from "../constants";
 
-const Pane = ({ fetchNotes, showPane, setShowPane }) => {
+const Create = ({ fetchNotes, showPane, setShowPane }) => {
   const onClose = () => setShowPane(false);
 
   return (
-    <NeetoUIPane isOpen={showPane} onClose={onClose}>
-      <NeetoUIPane.Header>
+    <Pane isOpen={showPane} onClose={onClose}>
+      <Pane.Header>
         <Typography style="h2" weight="semibold">
           Create a New Note
         </Typography>
-      </NeetoUIPane.Header>
+      </Pane.Header>
       <Form
         isEdit={false}
         note={NOTES_FORM_INITIAL_FORM_VALUES}
         refetch={fetchNotes}
         onClose={onClose}
       />
-    </NeetoUIPane>
+    </Pane>
   );
 };
 
-export default Pane;
+export default Create;
